d7a0f2c146c7582092403d07fbc526bc7448fb73
Yuriy Chumak <yuriy.chumak@mail.com>
2021-09-15 11:01:41 +0300

    changes according server

--- js/REST.js
+++ js/REST.js
@@ -37,17 +37,16 @@ function do_api_request(n, method, timestamp, headers, request, data) {
     arguments[0]++;
 	// console.log("new promise created (", session, ", ", timestamp, ")");
 	if (data != undefined)
-		console.info("try " + n + ", sending request: ", request, data);
+		console.info("try " + n + ", sending request: ", request.href, data);
 	else
-		console.info("try " + n + ", sending request: ", request);
+		console.info("try " + n + ", sending request: ", request.href);
 
     // новое поведение - на промисах
-    return fetch(API.host + request, {
+    return fetch(request, {
         method: method,
         headers: {
             ...headers,
             'Accept': 'application/json',
-            // 'Content-Type': 'application/json',
             'X-Joatmon-SID': $SESSION
         },
         body: data,
@@ -71,15 +70,32 @@ function do_api_request(n, method, timestamp, headers, request, data) {
 }
 
 // ----------------------------------------------------------------------
-GET = (request) => {
-    var method = "GET";
-    var headers = {};
-    var json = undefined;
-    return do_api_request(0, method, g_timestamp, headers, request, json);
+// GET = (request, data) => {
+// 	console.log("GET!!!", request)
+//     var method = "GET";
+//     var headers = (data == undefined) ? {} : {
+// 		'Content-Type': 'application/json',
+// 	};
+//     var json = (data == undefined) ? undefined : JSON.stringify(data);
+// 	console.log("headers: ", headers)
+// 	console.log("json: ", json)
+//     return do_api_request(0, method, g_timestamp, headers, request, json);
+// }
+
+GET = (request, data) => {
+	var method = "GET";
+	var request = new URL(API.host + request);
+	for (var property in data) {
+		request.searchParams.set(property, data[property]);
+	}
+	var headers = {};
+	var json = undefined;
+	return do_api_request(0, method, g_timestamp, headers, request, json);
 }
 
 PUT = (request, data) => {
     var method = "PUT";
+	var request = new URL(API.host + request);
     var headers = {
         'Content-Type': 'application/json',
     };
@@ -89,6 +105,7 @@ PUT = (request, data) => {
 
 POST = (request, data) => {
     var method = "POST";
+	var request = new URL(API.host + request);
     var headers = {
         'Content-Type': 'application/json',
     };
@@ -98,6 +115,7 @@ POST = (request, data) => {
 
 PATCH = (request, data) => {
     var method = "PATCH";
+	var request = new URL(API.host + request);
     var headers = {
         'Content-Type': 'application/json',
     };
